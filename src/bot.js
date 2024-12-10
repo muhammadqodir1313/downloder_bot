b9eb5092369152fbd24e1d594633a8a02fd4e60a
Muhammadqodir <farhodjonovichm1301@gmail.com>
2024-12-10 13:46:33 +0500

    O'zgarishlar qo'shildi

--- src/bot.js
+++ src/bot.js
@@ -4,10 +4,17 @@ import { handleYouTube, handleInstagram, handleTikTok, handleFacebook } from './
 import { isValidUrl, getPlatform } from './utils/validator.js';
 import { handleError } from './utils/error-handler.js';
 import { MESSAGES } from './config/messages.js';
+import express from 'express';
 
 dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
 
 bot.command('start', (ctx) => {
   ctx.reply(MESSAGES.WELCOME);
@@ -51,6 +58,10 @@ bot.on('text', async (ctx) => {
 
 process.on('unhandledRejection', (error) => {
   console.error('Unhandled promise rejection:', error);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 bot.launch();
